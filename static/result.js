c0858c9ae80d7d3e5cf34b3d3a5f0bb407a7d1ef
* <jvictory278@gmail.com>
2023-06-21 14:18:01 +0100

    i just made few changes to the main html and i workef on thr part that displays the resolution differently basically i dont like typing too much just check it out

---
+++ static/result.js
@@ -0,0 +1,71 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var vid_value = document.getElementById("res_input")
+    var res_con_ = document.getElementById("vid_value")
+    res_con_.textContent=vid_value.value   
+    var vid_ids = document.getElementById("vid_ids")  
+    list_ids = JSON.parse(vid_ids) 
+});
+
+function open_resbar() {
+    var resbar = document.getElementById("resbar")
+    resbar.classList.toggle("show")
+}
+var vid_info_bg = document.getElementById("vid_info_bg")
+function res_144p(res_id) {
+    var res_144 = document.querySelector(`div[vid-info-id="${res_id}"]`)
+    res_144.style.display="block";
+    res_144.style.display="flex";
+    var res_con = document.getElementById("vid_value")
+    res_con.textContent="144p"
+    
+    var vid_ids = document.getElementById("vid_ids").value
+    list_ids = JSON.parse(vid_ids)
+    
+    for (len_l in list_ids) {
+        if (list_ids[len_l]!=res_id) {
+            var rem = document.querySelector(`div[vid-info-id="${list_ids[len_l]}"]`)  
+            rem.style.display="none"  
+        }        
+    }   
+    var resbar = document.getElementById("resbar")
+    resbar.classList.toggle("show")    
+}
+function res_360p(res_id) {
+    var res_360 = document.querySelector(`div[vid-info-id="${res_id}"]`)
+    res_360.style.display="block";
+    res_360.style.display="flex";
+    var res_con1 = document.getElementById("vid_value")
+    res_con1.textContent="360p"
+    
+    var vid_ids = document.getElementById("vid_ids").value
+    list_ids = JSON.parse(vid_ids)
+    
+    for (len_l in list_ids) {
+        if (list_ids[len_l]!=res_id) {
+            var rem = document.querySelector(`div[vid-info-id="${list_ids[len_l]}"]`)  
+            rem.style.display="none"  
+        }        
+    }   
+    var resbar = document.getElementById("resbar")
+    resbar.classList.toggle("show")      
+}
+
+function res_720p(res_id) {
+    var res_720 = document.querySelector(`div[vid-info-id="${res_id}"]`)
+    res_720.style.display="block";
+    res_720.style.display="flex";
+    var res_con1 = document.getElementById("vid_value")
+    res_con1.textContent="720"
+    
+    var vid_ids = document.getElementById("vid_ids").value
+    list_ids = JSON.parse(vid_ids)
+    
+    for (len_l in list_ids) {
+        if (list_ids[len_l]!=res_id) {
+            var rem = document.querySelector(`div[vid-info-id="${list_ids[len_l]}"]`)  
+            rem.style.display="none"  
+        }        
+    }   
+    var resbar = document.getElementById("resbar")
+    resbar.classList.toggle("show")        
+}
